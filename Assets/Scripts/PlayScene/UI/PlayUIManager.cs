14ff9be0e304895cbb7206cc91a2626d543cab7b
NaTure77 <xnaudrornfl@gmail.com>
2020-01-01 17:57:42 +0900

    HomeImage 수정
    -맵 상 뒤로가기키 삭제
    -기존 스택구조 -> 버튼을 통해 맵과 맵간의 이동

--- Assets/Scripts/PlayScene/UI/PlayUIManager.cs
+++ Assets/Scripts/PlayScene/UI/PlayUIManager.cs
@@ -7,7 +7,7 @@ public class PlayUIManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public Image fadeBackground;
-
+    public GameObject currentPanel;
     public void Move(GameObject area)
     {
         StartCoroutine(Fade(area));
@@ -16,18 +16,20 @@ public class PlayUIManager : MonoBehaviour
     {
         Color tempColor = fadeBackground.color;
         float fadeTime = 0.2f;
-
+        fadeBackground.gameObject.SetActive(true);
         while (tempColor.a < 1f)
         {
             fadeBackground.color = tempColor;
             tempColor.a += Time.deltaTime / fadeTime;
             yield return null;
         }
-        tempColor.a = 1.0f;
+        tempColor.a = 1.0f;        
         fadeBackground.color = tempColor;
-        area.SetActive(!area.activeSelf);
-        tempColor = fadeBackground.color;
 
+        currentPanel.SetActive(false);
+        currentPanel = area;
+        currentPanel.SetActive(true);
+        
         while (tempColor.a > 0f)
         {
             fadeBackground.color = tempColor;
@@ -36,8 +38,8 @@ public class PlayUIManager : MonoBehaviour
         }
         tempColor.a = 0.0f;
         fadeBackground.color = tempColor;
+        fadeBackground.gameObject.SetActive(false);
     }
     // Update is called once per frame
    
-
 }
