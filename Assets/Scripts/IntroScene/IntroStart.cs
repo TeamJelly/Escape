2d8d7b9e3e178526384c2d61c29500fa9cd23a9e
Unagi11 <unagi11@naver.com>
2020-02-06 16:21:25 +0900

    Dialog.xml upload

--- Assets/Scripts/IntroScene/IntroStart.cs
+++ Assets/Scripts/IntroScene/IntroStart.cs
@@ -9,7 +9,7 @@ public class IntroStart : MonoBehaviour
     private void Start()
     {
         DataManager.Save();
-        ChatSystem2.instance.StartChat("I", 1, () =>
+        ChatSystem2.instance.StartChat("Intro", 1, () =>
          PlayUIManager.instance.FadeOutForNextScene(nextSceneName));
      }
 }
