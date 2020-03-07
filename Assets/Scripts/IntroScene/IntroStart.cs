a47e08a68a56dbe4352c8cebe1d669d885117f9e
NaTure77 <xnaudrornfl@gmail.com>
2020-03-07 23:55:57 +0900

    UI -> 카메라화 / 거실, 침실, 인트로 완료

--- Assets/Scripts/IntroScene/IntroStart.cs
+++ Assets/Scripts/IntroScene/IntroStart.cs
@@ -6,11 +6,11 @@ using System;
 public class IntroStart : MonoBehaviour
 {
     public string nextSceneName;
-    public CanvasGroup map;
+    public GameObject map;
     private void Start()
     {
 
-
+        map.SetActive(false);
         if (QuestDatabase.GetQusetState("[현관 진입]") < 1)
             ChatSystem2.instance.StartChat("Intro", "I1", () =>
               {
@@ -18,13 +18,15 @@ public class IntroStart : MonoBehaviour
                   ChatSystem2.instance.StartChat("Intro", "I2", () =>
                   {
                       PuzzleManager.instance.StartPuzzleWithName("베개바꿔치기");
-                // PlayUIManager.instance.FadeIn(map);
-                     DataManager.Save_Auto();
+                      map.SetActive(true);
+                      // PlayUIManager.instance.FadeIn(map);
+                      DataManager.Save_Auto();
+                     
                   });
               });
         else
         {
-            
+            map.SetActive(true);
             PuzzleManager.instance.StartPuzzleWithName("베개바꿔치기");
         }
     }
---
+++ Assets/Scripts/ScreenManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
+public class ScreenManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public SpriteRenderer Map;
+    public Light2D light2D;
+    private void Awake()
+    {
+        float spriteSizeRatio = Map.sprite.rect.size.y / Map.sprite.rect.size.x;
+        float needSizeX = Screen.height / spriteSizeRatio;
+        if (needSizeX > Screen.width)
+        {
+            float ratio = Screen.width / needSizeX;
+            Map.gameObject.transform.localScale = Vector3.one * ratio;
+            light2D.pointLightInnerRadius *= ratio;
+            light2D.pointLightOuterRadius *= ratio;
+        }
+    }
+}
