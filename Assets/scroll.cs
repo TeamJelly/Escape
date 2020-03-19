fd4d7a6664c386fa8224d92e07393690144fdd91
Unagi11 <unagi11@naver.com>
2020-03-19 21:08:27 +0900

    메인 메뉴 update

---
+++ Assets/scroll.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scroll : MonoBehaviour
+{
+    public float scrollSpeed;
+
+    private Renderer renderer;
+    private Vector2 savedOffset;
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        float x = Mathf.Repeat(Time.time * scrollSpeed, 1);
+        Vector2 offset = new Vector2(x, x);
+        renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+        
+    }
+}
